dd707f3a1ad02fcfb39e9f6c91ee0adef3a35ad8
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-05-11 15:11:55 +0200

    feat: add task Profile; user.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Profile } from './Profile/Profile';
+ import user from '../data/user.json';
+
+ 
+ export const App = () => {
+   return (
+     <div
+       style={{
+         height: '100%',
+         display: 'flex',
+         flexDirection: 'column',
+         justifyContent: 'center',
+         alignItems: 'center',
+       }}
+     >
+       <Profile
+         username={user.username}
+         tag={user.tag}
+         location={user.location}
+         avatar={user.avatar}
+         stats={user.stats}
+       />
+       {/* <Statistics stats={data} />
+       <FriendsList friends={friends} />
+       <TransactionHistory items={transactions} />; */}
+     </div>
+   );
+ };
