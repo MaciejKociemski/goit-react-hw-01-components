50a1ac530eff0134861b9c7189d22aa029a955a2
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2023-05-12 17:16:43 +0200

    feat: add friendList jsx and css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import { Profile } from './Profile/Profile';
-import { Statistics } from './Statistics/Statistics';
 import user from '../data/user.json';
+
+import { Statistics } from './Statistics/Statistics';
 import data from '../data/data.json';
 
- 
+import { FriendList } from './FriendList/FriendList';
+import friends from '../data/friends.json';
+
+
  export const App = () => {
    return (
      <div
@@ -23,8 +27,10 @@ import data from '../data/data.json';
          stats={user.stats}
        />
        <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends = {friends}/>
+
       
-       
      </div>
    );
  };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+
+
+const getBackgroundColor = variant => {
+  switch (variant) {
+    case true:
+      return 'green';
+    case false:
+      return 'red';
+    default:
+      throw new Error(`Unsupported variant prop value - ${variant}`);
+  }
+};
+
+export const FriendList = ({ friends }) => (
+  <>
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li className={css.item} key={id}>
+          <span
+            className={css.status}
+            style={{ backgroundColor: getBackgroundColor(isOnline) }}
+          ></span>
+          <img
+            className={css.avatar}
+            src={avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={css.name}>{name}</p>
+        </li>
+      ))}
+    </ul>
+  </>
+);
+
+FriendList.propTypes = {
+  friends: PropTypes.array,
+};
