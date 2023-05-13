be5f26f6bb8a47fdc628f037cd4ab0ae357c2b17
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2023-05-13 13:05:23 +0200

    fix: correction , remove inlining stool, add class to div container and take out class to index.css file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,23 +5,13 @@ import { TransactionHistory } from './Transactions/TransactionHistory';
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
-import transactions from '../data/transactions.json'
-
-
-
+import transactions from '../data/transactions.json';
 
 
  export const App = () => {
    return (
-     <div
-       style={{
-         height: '100%',
-         display: 'flex',
-         flexDirection: 'column',
-         justifyContent: 'center',
-         alignItems: 'center',
-       }}
-     >
+     <div className='container'>
+       
        <Profile
          username={user.username}
          tag={user.tag}
